727e10bfd958372eb368a5c35d16688c83e98c5b
Michael Borczuk <michaelborczuk@gmail.com>
2022-06-01 15:23:15 -0400

    matplotlib, newton

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from calcmath import CalcMath
 from term import *
 import math
+import matplotlib.pyplot as plt
 
-a = MultiTerm([NumTerm(5), PolyTerm(5, 5), PolyTerm(6, 6)])
-print(a.integral_term())
-print(a.integral_solve(1, 2))
-print(a.deriv_term())
-print(a.deriv_solve(1)
-print(a)
+poly = MultiTerm([PolyTerm(9, 2), NumTerm(-5)])
+# fig, ax = plt.subplots()
+# ax.spines['left'].set_position('center')
+# ax.spines['bottom'].set_position('center')
+# ax.spines['right'].set_color('none')
+# ax.spines['top'].set_color('none')
+# data = poly.graph()
+# x = data[0]
+# ax.set_xlim(xmin=-10, xmax=10)
+# y = data[1]
+# ax.set_ylim(ymin=-10, ymax=10)
+# ax.plot(x, y, linewidth=2)
+# plt.show()
+
+print(poly.newtonsmethod(2))
